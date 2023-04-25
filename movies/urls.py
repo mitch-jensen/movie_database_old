4aaec6584acb2e47021fbca696a89671d7ebc370
Mitch <mjensen98@gmail.com>
2023-04-25 14:58:57 +1000

    Started adding base list views

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from movies.views import HomePageView, AboutPageView
+from movies.views import HomePageView, AboutPageView, MovieListView, DistributorListView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
+    path("movies/", MovieListView.as_view(), name='all-movies'),
+    path("distributors/", DistributorListView.as_view(), name='all-distributors'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DetailView
+from django.views import View
+from movies.models import Movie, Distributor
 
 
 class HomePageView(TemplateView):
@@ -9,3 +11,21 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+class MovieBaseView(View):
+    model = Movie
+    fields = '__all__'
+    success_url = reverse_lazy('movies:all-movies')
+    
+
+class MovieListView(MovieBaseView, ListView):
+    pass
+
+class DistributorBaseView(View):
+    model = Distributor
+    fields = '__all__'
+    success_url = reverse_lazy('movies:all-distributors')
+    
+    
+class DistributorListView(DistributorBaseView, ListView):
+    pass

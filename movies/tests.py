212e7a1333c4e5629e47a5afea9ddba5a0a272d4
Mitch <mjensen98@gmail.com>
2023-04-25 15:24:22 +1000

    Added pytest and base test classes for each model

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+# from movies.models import PhysicalMedia, Distributor, Collection, Movie, Director
 
-# Create your tests here.
+class TestPhysicalMedia:
+    def test_medium(self):
+        assert 1 == 1
+
+class TestDistributor:
+    def test_distributor(self):
+        assert 1 == 1
+
+class TestCollection:
+    def test_collection(self):
+        assert 1 == 1
+
+class TestMovie:
+    def test_movie(self):
+        assert 1 == 1
+
+class TestDirector:
+    def test_director(self):
+        assert 1 == 1

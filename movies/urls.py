917a964f023e52778e4fb089f531241a8aaac880
Mitch <mjensen98@gmail.com>
2023-04-25 14:40:01 +1000

    Deleted default Pages app from DjangoX

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from ..pages.views import HomePageView, AboutPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),

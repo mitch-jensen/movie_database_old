899558b74ac18bc7ad784a9cb72281a93476b80f
Mitch <mjensen98@gmail.com>
2023-04-30 08:08:42 +1000

    Added Movie factory and initial test for Movie model.

--- movies/tests/factories.py
+++ movies/tests/factories.py
@@ -1,6 +1,9 @@
 import factory
+import random
+from datetime import date
 
-from movies.models import Director
+from movies.models import Director, Movie
+
 
 class DirectorFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -8,3 +11,27 @@ class DirectorFactory(factory.django.DjangoModelFactory):
 
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
+
+
+class MovieFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Movie
+
+    title = factory.Faker('sentence', nb_words=4, variable_nb_words=True)
+    release_date = factory.Faker(
+        'date_between_dates',
+        date_start=date(1890, 1, 1),
+        date_end=date.today()
+    )
+    watched = random.choice([True, False])
+
+    @factory.post_generation
+    def director(self, create, extracted, **kwargs):
+        if not create:
+            # Do nothing
+            return
+        
+        if extracted:
+            # A list of directors were passed in
+            for director in extracted:
+                self.director.add(director)
--- movies/tests/tests.py
+++ movies/tests/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from .factories import DirectorFactory
+from .factories import DirectorFactory, MovieFactory
 
 class TestPhysicalMedia:
     def test_medium(self):
@@ -14,8 +14,10 @@ class TestCollection:
         assert 1 == 1
 
 class TestMovie:
-    def test_movie(self):
-        assert 1 == 1
+    @pytest.mark.django_db
+    def test_str_method(self):
+        movie = MovieFactory()
+        assert str(movie) == movie.title + ' (' + str(movie.release_date.year) + ')'
 
 class TestDirector:
     @pytest.mark.django_db
